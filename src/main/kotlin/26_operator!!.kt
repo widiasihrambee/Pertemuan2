4f006b1c8c2a092aba96a3c9230c5c3142e8c442
RPL-NeliaDane <neliadane17@gmail.com>
2022-03-25 10:47:01 +0700

    percobaan  operator !!

---
+++ src/main/kotlin/26_operator!!.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+//Null safety
+//The !! operator
+    var maybeNumber: Int? = 15
+    println(maybeNumber!! * 2)
+    maybeNumber = null
+   // println(maybeNumber!! * 2)
+
+    // maka akan terjadi error
+}
