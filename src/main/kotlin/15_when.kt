a5c18531d938612ed8a2409281c8c3b4cb09e5f5
widiasihrambee <widiasihrambe@gmail.com>
2022-04-08 10:50:11 +0700

    Percobaan Pada When

--- src/main/kotlin/15_when.kt
+++ src/main/kotlin/15_when.kt
@@ -4,7 +4,9 @@ fun main() {
     when (x) {
         1 -> println("x == 1")
         2 -> println("x == 2")
-        else -> { // Note the black
+        else -> {
+            // Note the black
+
             println("x is naither 1 nor 2")
         }
 
@@ -12,8 +14,10 @@ fun main() {
     x = 2
     when (x) {
         1,2 -> println("value of x either 1,2")
-        else -> { //Note the block
-            println("x is neither 1 nor 2")
+        else -> {
+            //Note the blockk
+
+            println("x is neitherr 1 nor 2")
         }
     }
 
